f57b996ae31d0318641f49826b9ae95262730499
sagorahammed1 <sagorahammed002@gmail.com>
2025-10-27 22:20:39 +0600

    map screen done

--- lib/features/authentication/presentation/login_screen.dart
+++ lib/features/authentication/presentation/login_screen.dart
@@ -17,101 +17,106 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 50.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 50.h),
 
-              // Logo
-              Center(
-                child: Image.asset('assets/images/splash.png', height: 100.h),
-              ),
+                // Logo
+                Center(
+                  child: Image.asset('assets/images/splash.png', height: 100.h),
+                ),
 
-              SizedBox(height: 30.h),
+                SizedBox(height: 30.h),
 
-              // Welcome Text
-              Text(
-                "Welcome Back!",
-                style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w700),
-              ),
-
-              SizedBox(height: 40.h),
-
-              // Email Field
-              CustomAuthTextField(
-                controller: emailCtrl,
-                hintText: 'example@gmail.com',
-                keyboardType: TextInputType.emailAddress,
-              ),
-
-              SizedBox(height: 16.h),
-
-              // Password Field
-              CustomAuthTextField(
-                controller: passwordCtrl,
-                hintText: '************',
-                obscureText: true,
-                suffixIcon: Icon(Icons.visibility_off),
-              ),
-
-              SizedBox(height: 10.h),
-
-              // Forgot Password
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    // TODO: Add forgot password logic
-                    context.push('/forgotPassword');
-                  },
-                  child: Text(
-                    'Forgot Password',
-                    style: TextStyle(fontSize: 14.sp, color: Colors.blue),
+                // Welcome Text
+                Text(
+                  "Welcome Back!",
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-              ),
 
-              SizedBox(height: 60.h),
+                SizedBox(height: 40.h),
 
-              // Log In Button
-              CustomButton(
-                title: "Login",
-                onpress: () {
-                  print("tapped login");
-                  if (emailCtrl.text == "admin@gmail.com") {
-                    context.push("/adminBottomNavBar");
-                  } else {
-                    context.push('/bottomNavBar');
-                  }
-                },
-              ),
+                // Email Field
+                CustomAuthTextField(
+                  controller: emailCtrl,
+                  hintText: 'example@gmail.com',
+                  keyboardType: TextInputType.emailAddress,
+                ),
 
-              SizedBox(height: 20.h),
+                SizedBox(height: 16.h),
 
-              // Sign Up
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don’t Have An Account? ",
-                    style: TextStyle(fontSize: 14.sp),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      context.push('/sign-up');
+                // Password Field
+                CustomAuthTextField(
+                  controller: passwordCtrl,
+                  hintText: '************',
+                  obscureText: true,
+                  suffixIcon: Icon(Icons.visibility_off),
+                ),
+
+                SizedBox(height: 10.h),
+
+                // Forgot Password
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // TODO: Add forgot password logic
+                      context.push('/forgotPassword');
                     },
                     child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        fontSize: 14.sp,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.w600,
-                      ),
+                      'Forgot Password',
+                      style: TextStyle(fontSize: 14.sp, color: Colors.blue),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+
+                SizedBox(height: 60.h),
+
+                // Log In Button
+                CustomButton(
+                  title: "Login",
+                  onpress: () {
+                    print("tapped login");
+                    if (emailCtrl.text == "admin@gmail.com") {
+                      context.push("/adminBottomNavBar");
+                    } else {
+                      context.push('/bottomNavBar');
+                    }
+                  },
+                ),
+
+                SizedBox(height: 20.h),
+
+                // Sign Up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don’t Have An Account? ",
+                      style: TextStyle(fontSize: 14.sp),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        context.push('/sign-up');
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
